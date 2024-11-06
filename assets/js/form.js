e1ab2e98f4b24d499fc81d07bc470da19f5b4fa1
Rishikesh Sharma <rishikeshsharma828@gmail.com>
2024-11-06 12:38:11 +0530

    Your commit message here

--- assets/js/form.js
+++ assets/js/form.js
@@ -265,3 +265,17 @@ document.getElementById('payment-method').addEventListener('change', function() 
         paymentReferenceInput.style.display = 'none';
     }
 });
+
+
+
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycby5L8uKSC6llNDa6LSH4Uj3vc3eKZs-5qDwsE63r3si7dsN5M26fx25FutBFCQV9yfW/exec'
+            const form = document.forms['google-sheet']
+          
+            form.addEventListener('submit', e => {
+              e.preventDefault()
+              fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+                .then(response => alert("Thanks for Contacting us..! We Will Contact You Soon..."))
+                .catch(error => console.error('Error!', error.message))
+            })
